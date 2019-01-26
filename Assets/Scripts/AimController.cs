1b04918f83ba2558b0c345966bb397de274b4d0b
harhal19 <harhal19@mail.ru>
2019-01-26 20:11:32 +0300

    Added aimInput

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector3 OriginAimDirection = Vector3.right;
+
+    [SerializeField]
+    private GameObject Weapon;
+
+    private Quaternion _aimRotation = Quaternion.identity;
+    
+    public void AimToPoint(Vector3 TargetPoint)
+    {
+        var worldAimDirection = transform.position - TargetPoint;
+        var localAimDirection = ComplexMassController.GetRotatorForLocation(transform.position) * worldAimDirection;
+        
+        _aimRotation = Quaternion.FromToRotation(OriginAimDirection, localAimDirection);
+    }
+
+    public Quaternion GetAimRotation()
+    {
+        return _aimRotation;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, _aimRotation * transform.rotation * OriginAimDirection);
+    }
+}
---
+++ Assets/Scripts/AimInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AimController))]
+public class AimInput : MonoBehaviour
+{
+    private AimController _aimController;
+    private Camera _camera;
+    
+    private void Awake()
+    {
+        _aimController = GetComponent<AimController>();
+        _camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 aimPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+        _aimController.AimToPoint(aimPoint);
+    }
+}
