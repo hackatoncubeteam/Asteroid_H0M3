0fac327467cd9f33c8f11bcc174b9906182d8ea0
SuperGena <39839502+SuperGena@users.noreply.github.com>
2019-01-26 13:01:37 +0300

    Asteroid system

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     Rigidbody2D rb;
+    public float meteorSpeed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.left * 2, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector2.left * 2);
+        //float step = meteorSpeed * Time.deltaTime;
+        //transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.left, step);
     }
 }
