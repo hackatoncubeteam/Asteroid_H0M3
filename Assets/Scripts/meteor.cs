b9a0f4869b3a6b5f20c22a3849f76020f4360570
SuperGena <39839502+SuperGena@users.noreply.github.com>
2019-01-26 11:29:32 +0300

    Few files from gena

---
+++ Assets/Scripts/meteor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteor : MonoBehaviour
+{
+    Vector3 curMeteorPosition;
+    Vector3 planetPosition;
+    
+
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        planetPosition = GameObject.FindGameObjectWithTag("Planet").GetComponent<Transform>().position;
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+            Debug.Log("Meteor has no rigidbody");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var distance = Vector3.Distance(transform.position, planetPosition);
+        var sos = 0.5f / distance;
+
+        rb.AddForce((planetPosition - transform.position) * sos * 2, ForceMode2D.Force);
+    }
+}
