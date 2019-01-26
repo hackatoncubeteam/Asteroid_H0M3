c9927dd775dd11d228b1276e2292f5e927cde734
SuperGena <39839502+SuperGena@users.noreply.github.com>
2019-01-26 18:03:26 +0300

    Meteor System update

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,17 +6,33 @@ public class Move : MonoBehaviour
 {
     Rigidbody2D rb;
     public float meteorSpeed;
+    Vector2 moonPosition;
+
+    float rotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.left * 2, ForceMode2D.Impulse);
+        //moonPosition = .GetComponent<Transform>().position;
+        //rb = GetComponent<Rigidbody2D>();
+        //
+        //rotation = Random.Range(-vectorRotationAngle, vectorRotationAngle);
+        //
+        //ToTheMoon();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //float step = meteorSpeed * Time.deltaTime;
-        //transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.left, step);
+    
+    void ToTheMoon(Vector3 Direction)
+    {        
+        //float randAng = Random.Range(2, 4);
+        //
+        //Vector2 curPos = transform.position;
+        //
+        //Vector3 forceDirection = (moonPosition - curPos).normalized;
+        //forceDirection = Quaternion.Euler(0, 0, randAng) * forceDirection;
+        //
+        //rb.AddForce(moonPosition - curPos, ForceMode2D.Impulse);
+        //
+        //rb.velocity *= 0.5f;
     }
 }
