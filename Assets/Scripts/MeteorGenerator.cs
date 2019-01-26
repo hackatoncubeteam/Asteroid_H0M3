49c658412e258edd4348262d42ea205e84160158
SuperGena <39839502+SuperGena@users.noreply.github.com>
2019-01-26 14:09:29 +0300

    Bezier Curve

---
+++ Assets/Scripts/MeteorGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeteorGenerator : MonoBehaviour
+{
+    BezierCurve bc;
+
+    List<Transform> meteorSpawnPosition;
+
+    GameObject meteor;
+
+    float timer;
+
+
+    Random rnd;
+
+    void Start()
+    {
+        bc = GetComponent<BezierCurve>();
+        //if()
+    }
+
+    //void SpawnMeteor()
+    //{
+    //    Instantiate(meteor, Random.)
+    //}
+}
