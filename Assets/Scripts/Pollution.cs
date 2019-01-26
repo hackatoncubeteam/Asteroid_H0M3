a4c344c6941d2657055a728fa184033cf0adf172
harhal19 <harhal19@mail.ru>
2019-01-27 02:45:50 +0300

    Added pollution

---
+++ Assets/Scripts/Pollution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollution : MonoBehaviour
+{
+    [SerializeField, Range(0, 100)]
+    private float PollutionLevel;
+
+    public void SetPollutionLevel(float pollutionLevel)
+    {
+        PollutionLevel = pollutionLevel;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
