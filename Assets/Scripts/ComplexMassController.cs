7bb3e6a6f26b282e01df17aac6e1d970ff29246b
harhal19 <harhal19@mail.ru>
2019-01-26 13:53:16 +0300

    Added Camera controller

---
+++ Assets/Scripts/ComplexMassController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComplexMassController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Rigidbody2D[] GetAllIncludedBodies()
+    {
+        return GetComponentsInChildren<Rigidbody2D>();
+    }
+
+    public Vector2 GetCenterOfMass()
+    {
+        Vector2 OutCenterOfMass = new Vector2();
+        float Mass = 0;
+        
+        foreach (Rigidbody2D Body in GetAllIncludedBodies())
+        {
+            OutCenterOfMass = OutCenterOfMass * Mass + Body.worldCenterOfMass * Body.mass;
+            
+            Mass += Body.mass;
+
+            if (Mass > 0)
+            {
+                OutCenterOfMass /= Mass;
+            }
+        }
+        
+        return OutCenterOfMass;
+    }
+
+    public float GetMass()
+    {
+        float Mass = 0;
+        
+        foreach (Rigidbody2D Body in GetAllIncludedBodies())
+        {
+            Mass += Body.mass;
+        }
+        
+        return Mass;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(GetCenterOfMass(), 1f);
+    }
+}
