dca41695f02b3005df46766af0076d96aa7c0a7b
SuperGena <39839502+SuperGena@users.noreply.github.com>
2019-01-27 17:25:44 +0300

    scrypt to player deth

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    //TODO ref to global scrypt
+
+    bool isGameOver;
+
+    void OnTriggerEnter2D(Collision2D col)
+    {
+        isGameOver = true;
+    }
+}
